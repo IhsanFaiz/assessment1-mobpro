4e0e1c85fc614531bb1033b81fc4849eea7e018c
Ihsan Faiz <ihsanfaiz200@gmail.com>
2026-04-07 09:47:53 +0700

    membuat content bola

---
+++ app/src/main/java/com/ihsanfaiz0048/assesment1_mobpro/ui/screen/contentBangunRuang/BolaContent.kt
@@ -0,0 +1,142 @@
+package com.ihsanfaiz0048.assesment1_mobpro.ui.screen.contentBangunRuang
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import com.ihsanfaiz0048.assesment1_mobpro.R
+import com.ihsanfaiz0048.assesment1_mobpro.model.BangunRuang
+import com.ihsanfaiz0048.assesment1_mobpro.model.HasilBangunRuang
+import com.ihsanfaiz0048.assesment1_mobpro.ui.screen.ErrorHint
+import com.ihsanfaiz0048.assesment1_mobpro.ui.screen.IconPicker
+
+@Composable
+fun BolaContent(bangunRuang: BangunRuang){
+    var jari by rememberSaveable { mutableStateOf("") }
+    var hasilBangunRuang by remember { mutableStateOf<HasilBangunRuang?>(null) }
+    var jariImage by rememberSaveable { mutableFloatStateOf(0F) }
+    var jariError by rememberSaveable { mutableStateOf(false) }
+
+    OutlinedTextField(
+        value = jari,
+        onValueChange = { jari = it },
+        label = { Text(text = stringResource(R.string.jariJari)) },
+        trailingIcon = { IconPicker(jariError) },
+        supportingText = { ErrorHint(jariError) },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Number,
+            imeAction = ImeAction.Next
+        ),
+        modifier = Modifier.fillMaxWidth()
+    )
+    Spacer(modifier = Modifier.padding(top = 16.dp))
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            Button(
+                onClick = {
+                    jariError = (jari == "" || jari == "0")
+                    if (jariError) return@Button
+                    val r = jari.toFloatOrNull()
+                    if (r != null){
+                        hasilBangunRuang = bangunRuang.hitung(listOf(r))
+                        jariImage = r
+                    }
+                },
+                modifier = Modifier
+            ) {
+                Text(
+                    text = stringResource(R.string.hitung)
+                )
+            }
+            Button(
+                onClick = {
+                    jari = ""
+                    hasilBangunRuang = null
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.error
+                )
+            ) {
+                Text(
+                    text = stringResource(R.string.reset)
+                )
+            }
+        }
+        Spacer(modifier = Modifier.padding(top = 16.dp))
+        if (hasilBangunRuang?.volume != null){
+            HorizontalDivider(
+                modifier = Modifier.padding(vertical = 8.dp),
+                thickness = 1.dp
+            )
+            Spacer(modifier = Modifier.padding(top = 16.dp))
+            Text(
+                text = stringResource(R.string.volume, hasilBangunRuang?.volume?: 0.0),
+                style = MaterialTheme.typography.headlineSmall
+            )
+            Spacer(modifier = Modifier.padding(top = 16.dp))
+            Text(
+                text = stringResource(R.string.luas_permukaan, hasilBangunRuang?.luasPermukaan?: 0.0),
+                style = MaterialTheme.typography.headlineSmall
+            )
+            Spacer(modifier = Modifier.padding(top = 16.dp))
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ){
+                Icon(
+                    painter = painterResource(R.drawable.bola),
+                    contentDescription = stringResource(R.string.bola),
+                    modifier = Modifier.size(300.dp),
+                    tint = MaterialTheme.colorScheme.onBackground
+                )
+                Text(
+                    text = stringResource(R.string.jariJari) + ": " + jariImage,
+                    modifier = Modifier.padding(start = 100.dp).background(
+                        color = MaterialTheme.colorScheme.onBackground,
+                        shape = RoundedCornerShape(8.dp)
+                    ).padding(6.dp),
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold,
+                )
+            }
+        }
+    }
+}
